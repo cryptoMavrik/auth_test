2b13796a6b44c6a4a5c2ed297214d850b6ffb5bf
cryptoMavrik <xdaegonx@gmail.com>
2022-08-11 21:08:49 -0400

    remove stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,6 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { useSession, signIn, signOut } from 'next-auth/react';
 
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
   const { data: session, status } = useSession();
@@ -28,37 +22,30 @@ const Home: NextPage = () => {
           Auth <span className="text-teal-400">Test</span>
         </h1>
         <div className="pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          {!session && <button className="px-[2rem] py-[.5rem] bg-teal-400 rounded-xl text-gray-600 font-bold"
-            onClick={() => signIn()}>
-            Login
-          </button>}
+          {
+            !session &&
+            <button className="px-[2rem] py-[.5rem] bg-teal-400 rounded-xl text-gray-600 font-bold"
+              onClick={() => signIn()}>
+              Login
+            </button>
+          }
+          {session &&
+            <>
+              <p>
+                <span className="font-bold">{session.user ? session.user.name : "Loading..."}</span>
+              </p>
+              <button className="px-[2rem] py-[.5rem] bg-teal-400 rounded-xl text-gray-600 font-bold"
+                onClick={() => signOut()}>
+                Logout
+              </button>
+            </>
+          }
         </div>
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
       </main>
     </>
-  );
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
   );
 };
 
